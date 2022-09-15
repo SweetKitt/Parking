46cc7e88c1ff78a5f14b7be83e15b3e619dfa5ba
SweetKitt <Faite@bk.ru>
2022-09-15 22:15:20 +0300

    Починила редактор машины.Теперь работает быстрое добавление клиента и авто на парковку через главную страницу.

--- main/urls.py
+++ main/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('creation', views.creation, name='creation'),
-    path('creation_car', views.creation_car, name='creation_car'),
-    path('editor/<int:pk>/', views.EditorView.as_view(), name='editor'),
+    path('creation', views.create_client, name='creation'),
+    path('creation_car', views.create_car, name='creation_car'),
+    path('editor/<int:id>/', views.car_editing, name='editor'),
     path('editor_client/<int:pk>/', views.EditorClientView.as_view(), name='editor_client'),
-    path('delete/<int:id>/', views.delete, name='delete'),
+    path('delete/<int:id>/', views.delete_car, name='delete'),
     path('delete_client/<int:id>/', views.delete_client, name='delete_client'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -12,37 +12,59 @@ def home(request):
     paginator = Paginator(cars, 3)
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
+    if request.method == 'POST':
+        brand = request.POST.get('car')
+        car = Car.objects.get(id=brand)
+        car.is_on_parking = 'True'
+        car.save()
     return render(request, 'main/home.html', {'home': 'Все клиенты', 'clients': clients, 'cars': cars, 'page_obj': page_obj})
 
 
-def creation(request):
+def create_client(request):
     if request.method == 'POST':
         form = ClientForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('creation_car')
+        else:
+            return render(request, 'main/creation.html', {'form': form})
     else:
         form = ClientForm()
     form = ClientForm()
     return render(request, 'main/creation.html', {'form': form})
 
 
-def creation_car(request):
+def create_car(request):
     if request.method == 'POST':
         form2 = CarForm(request.POST)
         if form2.is_valid():
             form2.save()
             return redirect('/')
+        else:
+            return render(request, 'main/creationcar.html', {'form2': form2})
     else:
         form2 = CarForm()
     form2 = CarForm()
     return render(request, 'main/creationcar.html', {'form2': form2})
 
 
-class EditorView(UpdateView):
-    model = Car
-    template_name = 'main/editor.html'
-    form_class = CarForm
+# class EditorView(UpdateView):
+#     model = Car
+#     template_name = 'main/editor.html'
+#     form_class = CarForm
+
+def car_editing(request, id):
+    car = Car.objects.get(id=id)
+    if request.method == 'POST':
+        car.brand = request.POST.get('brand', car.brand)
+        car.model = request.POST.get('model', car.model)
+        car.color = request.POST.get('color', car.color)
+        car.licence_plate = request.POST.get('licence_plate', car.licence_plate)
+        car.is_on_parking = request.POST.get('is_on_parking', False)
+        car.save()
+        return HttpResponseRedirect('/')
+    else:
+        return render(request, "main/editor.html", {"car": car})
 
 
 class EditorClientView(UpdateView):
@@ -51,20 +73,22 @@ class EditorClientView(UpdateView):
     form_class = ClientForm
 
 
-def delete(request, id):
+def delete_car(request, id):
     try:
         car = Car.objects.get(id=id)
-        car.delete()
-        return HttpResponseRedirect("/")
     except Car.DoesNotExist:
         return HttpResponseNotFound("<h2>Запись не найдена</h2>")
+    car.delete()
+    return HttpResponseRedirect("/")
+
 
 
 def delete_client(request, id):
     try:
         client = Client.objects.get(id=id)
-        client.delete()
-        return HttpResponseRedirect("/")
     except Car.DoesNotExist:
         return HttpResponseNotFound("<h2>Запись не найдена</h2>")
+    client.delete()
+    return HttpResponseRedirect("/")
+
 
